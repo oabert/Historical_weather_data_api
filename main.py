7a1e9cc16c06c7d5abc1180f1529f865818af855
Olena Abert <oabert@gmail.com>
2023-12-05 14:11:08 -0500

    Add dictionary api data to main page. Fin project

--- main.py
+++ main.py
@@ -33,6 +33,24 @@ def weather(station, date):
     return temperature_dict
 
 
+# @app.route('/api/v1/<station>')
+# def stations(station):
+#     filepath = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+#     df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+#     result = df.to_dict(orient='records')
+#     return result
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filepath = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filepath, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
+
+
+# Dictionary part
 filepath_d = 'dictionary.csv'
 df = pd.read_csv(filepath_d)
 
